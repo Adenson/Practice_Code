9cec5b2c019c969a72c78ffae4ae64ce1e41e9de
Adenson <15594522896@163.com>
2019-12-06 15:25:31 +0800

    另类加法

--- practice_17_另类加法 .cpp
+++ practice_17_另类加法 .cpp
@@ -14,6 +14,25 @@ public:
 			A = add;
 			B = tmp;
 		}
-		return add;
+		return A;
 	}
 };
+
+//这个应该也算是比较巧妙吧
+public class Solution
+ {
+    public int Add(int num1,int num2)
+	{
+        if(num1>0)
+		{
+            while(num1--!=0)
+                num2++;
+        }
+        else if(num1<0)
+		{
+            while(num1++!=0)
+                num2--;
+        }
+        return num2;
+    }
+}
