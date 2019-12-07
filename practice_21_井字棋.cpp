23ad5a402c826f0234d2c3842516b16176909b2b
Adenson <15594522896@163.com>
2019-12-07 22:55:00 +0800

    井字棋

--- practice_21_井字棋.cpp
+++ practice_21_井字棋.cpp
@@ -1,7 +1,7 @@
 题目ID：24992 --井字棋 
 链接：https://www.nowcoder.com/practice/e1bb714eb9924188a0d5a6df2216a3d1?tpId=8 &&tqId=11055&rp=1&ru=/activity/oj&qru=/ta/cracking-the-coding-interview/question-ranki ng
 【题目解析】： 
-井字棋，是一种在3*3格子上进行的连珠游戏，三个相同就代表获胜。
+井字棋，是一种在3*3格子上进行的连珠游戏，三个相同就代表获胜。 
 
 class Board {
 public:
