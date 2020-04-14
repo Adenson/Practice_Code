14c944d5e03be66b5c7160af3a7a5c73c27d1c2a
Adenson <15594522896@163.com>
2020-04-14 22:28:22 +0800

    提交代码

---
+++ 链表中倒数第k个节点.cpp
@@ -0,0 +1,69 @@
+输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。例如，一个链表有6个节点，从头节点开始，它们的值依次是1、2、3、4、5、6。这个链表的倒数第3个节点是值为4的节点。
+示例：
+给定一个链表: 1->2->3->4->5, 和 k = 2.
+返回链表 4->5.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof
+
+class Solution {
+public:
+    ListNode*& reverse(ListNode*& head)
+    {
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
+        while(head)
+        {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next; 
+        } 
+        return prev;
+    }
+    ListNode* getKthFromEnd(ListNode* head, int k) 
+    {
+        ListNode* tmp = reverse(head);
+        ListNode* first = tmp;
+        for(int i = 0; i < k - 1; i++)
+        {
+            first = first->next;
+        }
+        first->next = nullptr;
+
+        return reverse(tmp);
+    }
+};
+
+class Solution {
+public:
+    ListNode* getKthFromEnd(ListNode* head, int k) 
+    {
+        //一定要值得考虑
+        if(head == nullptr || k == 0){
+            return nullptr;
+        }
+
+        ListNode* fastptr = head;
+        ListNode* slowptr = head;
+        while(k-1)
+        {
+            //防止K大于结点个数造成空指针访问错误
+            if(fastptr->next != nullptr){
+                 fastptr = fastptr->next;
+                 k--; 
+            }
+            else{
+                return nullptr;
+            }
+        }
+        while(fastptr->next != nullptr)
+        {
+            fastptr = fastptr->next;
+            slowptr = slowptr->next;
+        }
+
+        return slowptr;
+    }
+};
+
